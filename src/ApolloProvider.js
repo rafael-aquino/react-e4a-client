f0d56c4d4d0396150ecbcee3474207e0817ab0fe
Rafael Aquino <rafael.aquinoquijano@gmail.com>
2021-03-09 16:30:05 -0600

    apollo client basic config

---
+++ src/ApolloProvider.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import App from "./App";
+const client = new ApolloClient({
+  uri: "http://localhost:5000",
+  cache: new InMemoryCache(),
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
